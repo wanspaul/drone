027a9a43d042daebc8ded7823aa5d2d0d1c00dcc
wanspaul <wanspaul@gmail.com>
2017-08-26 15:36:43 +0900

    hostname 변경

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 import socket
 
 if __name__ == "__main__":
-    if socket.gethostname() == 'ec2-52-78-93-33.ap-northeast-2.compute.amazonaws.com':
+    if socket.gethostname() == 'ec2-52-78-93-33.ap-northeast-2.compute.amazonaws.com' or socket.gethostname('ip-172-31-2-13'):
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drone.settings.production")
     else:
         os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drone.settings.local")
