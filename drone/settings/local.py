cfe7848f58f1546399c67d59c8fbb745cae05461
wanspaul <wanspaul@gmail.com>
2017-08-26 15:33:33 +0900

    리얼 환경 세팅

--- drone/settings/local.py
+++ drone/settings/local.py
@@ -3,3 +3,10 @@ from drone.settings.base import *
 MONGO_HOST = '127.0.0.1'
 MONGO_PORT = 27018
 PASSWORD = '1234'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
